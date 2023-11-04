651ecbbfe7f0651e840b134d70b7f2cdd365e53d
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2023-11-04 16:05:10 +0530

    ui changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <html lang="en" suppressHydrationWarning>      
         <body
           className={cn(
-            "min-h-screen bg-background font-sans antialiased",
+            "min-h-screen bg-white font-sans antialiased",
             fontSans.variable
           )}
         >
